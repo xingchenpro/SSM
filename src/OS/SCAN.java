afa1c6de048d7b3e22ac7a4187e28ce6051a6317
黄良运 <1136513099@qq.com>
2018-06-24 23:45:45 +0800

    SSTF

---
+++ src/OS/SCAN.java
@@ -0,0 +1,25 @@
+package OS;
+
+/**
+ * @author :hly
+ * @date :2018/6/24
+ */
+public class SCAN {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String [] args){
+
+    }
+}
--- src/OS/SSTF.java
+++ src/OS/SSTF.java
@@ -8,46 +8,71 @@ import java.util.*;
  */
 public class SSTF {
     static Scanner scanner = new Scanner(System.in);
+    //请求序列长度
+    static int length;
     //存放序列
-    static List<Integer> list;
+    static int[] arrays;
     //最短路径
-    static List<Integer> listAfter;
+    static int[] arraysAfter;
     //经过的最短长度
     static int sumLength = 0;
+    //开始位置
+    static int start;
+
+    static int MIM_NUM = Integer.MAX_VALUE;
+
+    //输入
+    public static void input(){
+        System.out.println("请输入请求序列的长度:");
+        length = scanner.nextInt();
+        arrays = new int[length];
+        arraysAfter = new int[length];
+        System.out.println("请输入请求的序列:");
+        for (int i = 0; i < length; i++)
+            arrays[i] = scanner.nextInt();
+        System.out.println("请输入开始扫描的位置:");
+        start = scanner.nextInt();
+    }
 
     //最短优先SSTF
     public static void SSTF() {
+        int num = length;
+        int thisMinPoint = start;
+        int thisIndex = 0;
+        int after = 0;
 
-        System.out.println("请输入请求序列的长度:");
-        int length = scanner.nextInt();
-        list = new ArrayList<Integer>(length);
-        listAfter = new ArrayList<Integer>(length);
-        System.out.println("请输入请求的序列:");
-        for (int i = 0; i < length; i++)
-            list.add(scanner.nextInt());
-        System.out.println("请输入开始扫描的位置:");
-        int start = scanner.nextInt();
+        while (num > 0) {
+            for (int j = 0; j < length; j++) {
+                if (Math.abs(thisMinPoint - arrays[j]) < MIM_NUM && arrays[j] != -1) {
+                    MIM_NUM = Math.abs(thisMinPoint - arrays[j]);
+                    thisIndex = j;
+                }
+            }
+            num--;
+            thisMinPoint = arrays[thisIndex];
+            arraysAfter[after++] = arrays[thisIndex];
+            arrays[thisIndex] = -1;
+            sumLength += MIM_NUM;
+            MIM_NUM = Integer.MAX_VALUE;
 
-        //排序
-        Collections.sort(list);
-
-        while(list.size()>0){
-            int temp = start;
-            int thisMin=Math.abs(list.get(temp)-1)<Math.abs(list.get(temp)+1)?list.get(temp-1):list.get(temp+1);
-            list.remove(temp);
-            listAfter.add(temp);
-            temp = thisMin;
-            sumLength+=thisMin;
         }
-        System.out.println("最短的长度为:"+sumLength);
-        //最短路径序列为
-        for(Integer i:listAfter)
-            System.out.print(i+" ");
-
     }
 
-    public static void main(String[]args){
+    //输出结果
+    public static void output(){
+        System.out.println("总路线长度为:" + sumLength);
+        System.out.println("平均查找长度为:" + sumLength / length);
+        System.out.println("查找路径为:");
+        for (int i : arraysAfter)
+            System.out.print(i + " ");
+    }
+
+    public static void main(String[] args) {
+        //98，183，37，122，14，124，65，67
+        input();
         SSTF();
+        output();
+
     }
 
 
