95bd89967e68fa4df31b523d06367b0e7fe150b2
黄良运 <1136513099@qq.com>
2018-08-15 19:05:46 +0800

    完成图片上传

---
+++ MySSM/src/main/java/com/hly/ssm/controller/fileUploadController/EditormdFileUploadController.java
@@ -0,0 +1,74 @@
+package com.hly.ssm.controller.fileUploadController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
+
+/**
+ * @author :hly
+ * @github :github.com/SiriusHly
+ * @blog :blog.csdn.net/Sirius_hly
+ * @date :2018/8/15
+ */
+@Controller
+@RequestMapping(value = "/fileUpload" )
+public class EditormdFileUploadController {
+
+
+    @RequestMapping(value = "/editorMDUpload" ,method = RequestMethod.POST)
+    public void editorMD(HttpServletRequest request, HttpServletResponse response,@RequestParam(value = "editormd-image-file", required = false) MultipartFile file) throws IOException {
+        try {
+            request.setCharacterEncoding( "utf-8" );
+            /*这里返回格式是html/txt才能上传*/
+            response.setHeader( "Content-Type" , "application/json" );
+            //获得web项目的全路径
+            String rootPath = request.getSession().getServletContext().getRealPath("/resource/upload/");
+            //Calendar.getInstance()是获取一个Calendar对象并可以进行时间的计算，时区的指定
+            Calendar date = Calendar.getInstance();
+            //获得日期路径,MONTH个值的初始值是0，因此我们要用它来表示正确的月份时就需要加1。
+            File dateFile = new File(date.get(Calendar.YEAR)+"/"+(date.get(Calendar.MONTH)+1)+"/"+(date.get(Calendar.DATE)));
+            //获得文件最初的路径
+            String originalFile = file.getOriginalFilename();
+            //得到完整路径名
+            File newFile = new File(rootPath+File.separator+dateFile+File.separator+originalFile);
+
+            /*文件不存在就创建*/
+            if(!newFile.getParentFile().exists()){
+                newFile.getParentFile().mkdirs();
+            }
+            //String url = rootPath+"/"+dateFile+"/"+newFile;
+            //System.out.println("url:"+url);
+            //file.getOriginalFilename()得到最初的文件名
+            file.transferTo(newFile);
+            //System.out.println("/resources/upload/"+dateFile+File.separator+originalFile);
+            System.out.println(dateFile+"/"+file.getOriginalFilename());
+            /*JSON格式*/
+            response.getWriter().write("{\"success\":1,\"message\":\"上传成功\",\"url\":\"/resource/upload/"+date.get(Calendar.YEAR)+"/"+(date.get(Calendar.MONTH)+1)+"/"+date.get(Calendar.DATE)+"/"+file.getOriginalFilename()+"\"}");
+
+
+        } catch (Exception e) {
+            response.getWriter().write("{\"success\":0}");
+
+        }
+
+    }
+    @RequestMapping("editorMD")
+    public ModelAndView editormd(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("/editor/simple");
+        return mv;
+    }
+
+
+
+}
--- MySSM/src/main/webapp/WEB-INF/jsp/editor/simple.jsp
+++ MySSM/src/main/webapp/WEB-INF/jsp/editor/simple.jsp
@@ -1,10 +1,20 @@
-<!DOCTYPE html>
+<%--
+  Created by IntelliJ IDEA.
+  User: hly
+  Date: 2018/8/15
+  Time: 16:17
+  github :github.com/SiriusHly
+  blog :blog.csdn.net/Sirius_hly
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<% String path = request.getContextPath();%>
 <html lang="zh">
 <head>
     <meta charset="utf-8"/>
     <title>Simple example - Editor.md examples</title>
-    <link rel="stylesheet" href="css/style.css"/>
-    <link rel="stylesheet" href="../css/editormd.css"/>
+    <link rel="stylesheet" href="<%=path%>/static/html/editor/editor.md-master/examples/css/style.css"/>
+    <link rel="stylesheet" href="<%=path%>/static/html/editor/editor.md-master/css/editormd.css"/>
     <link rel="shortcut icon" href="https://pandao.github.io/editor.md/favicon.ico" type="image/x-icon"/>
 </head>
 <body>
@@ -17,8 +27,8 @@
         <textarea class="editor-md-html" name="editor-md-html"></textarea>
     </div>
 </div>
-<script src="js/jquery.min.js"></script>
-<script src="../editormd.min.js"></script>
+<script src="<%=path%>/static/html/editor/editor.md-master/examples/js/jquery.min.js"></script>
+<script src="<%=path%>/static/html/editor/editor.md-master/editormd.min.js"></script>
 <script type="text/javascript">
     var testEditor;
 
@@ -27,14 +37,17 @@
             width: "70%",//编辑器大小官方默认90%
             height: 640,
             syncScrolling: "single",
-            path: "../lib/",
+            path: "<%=path%>/static/html/editor/editor.md-master/lib/",
             saveHTMLToTextarea : true,//注意3：这个配置，textarea可以提交
 
             emoji: true,//emoji表情，默认关闭
             /**上传图片相关配置如下*/
             imageUpload : true,
             imageFormats : ["jpg", "jpeg", "gif", "png", "bmp", "webp"],
-            imageUploadURL : "/editorMDUpload",//注意你后端的上传图片服务地址
+            imageUploadURL : "<%=path%>/fileUpload/editorMDUpload",//注意你后端的上传图片服务地址
+            onload: function () { //上传成功之后的回调
+
+            },
 
 
             //editorTheme: "pastel-on-dark",//编辑器的主题颜色
